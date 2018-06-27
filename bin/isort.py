7c04d2466d713494c8eb4a5710a15178a18f5b57
Serg Baburin <serg.baburin@gmail.com>
2018-06-27 13:43:28 +0300

    Fix and close #96

--- bin/isort.py
+++ bin/isort.py
@@ -5,12 +5,20 @@ import os
 import re
 import sys
 
+
 sys.path.insert(0, os.path.join(
     os.path.abspath(os.path.dirname(__file__)),
     'deps'
 ))
-from isort.main import main
 
+if sys.version_info[0] == 2:
+	sys.path.insert(0, os.path.join(
+	    os.path.abspath(os.path.dirname(__file__)),
+	    'backports'
+	))
+
+
+from isort.main import main
 
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
